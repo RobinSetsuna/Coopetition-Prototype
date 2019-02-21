fc0fdc0fd79474b36031c5c807f68ef82a43284d
Haoyu Guo (RIT Student) <hg7364@ad.rit.edu>
2019-02-21 15:12:37 -0500

    Merge the simple controller with player class
    Should make the later work easier when you really dig into the ingame interaction

--- Jam5-Prototype/Assets/Scripts/Player.cs
+++ Jam5-Prototype/Assets/Scripts/Player.cs
@@ -1,36 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum playerState {
+    Moveable = 1,
+    Seated,
+    Boosting,
+    Carrying,
+    Default
+   }
 public class Player : MonoBehaviour {
+    [SerializeField] private float speed;
+    [SerializeField] private float boostStrength;
+    [SerializeField] private float boostFreezeDuration;
 
-    [SerializeField] private bool boosting = false;
-    [SerializeField] private bool onChair = false;
-    [SerializeField] private bool carrying = false;
-    [SerializeField] private float sightRadius = 5.0f;
-    [SerializeField] private int playerIndex;
-    [SerializeField] private SimpleController myInput;
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool carrying;
+    [SerializeField] private float sightRadius;
 
-	// Use this for initialization
-	void Start () {
-        myInput = new SimpleController();
-        rb = myInput.GetRigidbody();        
+    [SerializeField] private int index;
+    private Rigidbody2D rb2d;
+    private playerState currentState;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        currentState = playerState.Moveable;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-      if(onChair)
-        {
-            myInput.SetDisabled(true);
-        }
-	}
+        switch (currentState) {
+            case playerState.Moveable:
+                float h = Input.GetAxis("Horizontal" + index);
+                float v = Input.GetAxis("Vertical" + index);
+                rb2d.velocity = new Vector2(h * speed, v * speed);
+                if (Input.GetButtonDown("Boost" + index)) {
+                    //boosting
+                    // TODO may add another code here
+                    Boost();
+                }
+                break;
+            case playerState.Boosting:
+                // when boosting, freeze all input
+                break;
 
-   public void SetBoosting(bool boost) { boosting = boost; myInput.SetBoosting(boosting); }
-   public void SetIndex(int index) { playerIndex = index; }
-   public int GetIndex() { return playerIndex; }
+            case playerState.Carrying:
+                // I think for now using the moveable for carrying will be fine
+                break;
+            case playerState.Seated:
+                // for now ,do nothing
+                break;
+        } 
+	}
+    public void Initialize(Vector3 position,int _index = -1)
+    {
+        index = _index == -1 ? index : _index;
+        currentState = playerState.Moveable;
+        transform.position = position;
+    }
+
+    public void Boost() {
+
+        // get current move direction
+        var temp = rb2d.velocity;
+        var currentDirection = temp.normalized;
+        rb2d.AddForce(currentDirection * boostStrength);
+        StartCoroutine(releaseToIdle(boostFreezeDuration));
+    }
+    public void Carry() {
+        speed = 1.5f;
+        boostFreezeDuration = boostFreezeDuration * 2;
+    }
+
+   public int GetIndex() { return index; }
+
    public void SetRadius(int overlapped)
     {
         sightRadius = 5.0f + ((1 + overlapped) * .25f); //Line of sight radius is increased based on how many other players are within range of this player 
+    }
+
+
+    IEnumerator releaseToIdle(float duration) {
+        yield return new WaitForSeconds(duration);
+        if (currentState != playerState.Seated) {
+            currentState = playerState.Moveable;
+            // release the freezing
+        }
     }
 }
--- Jam5-Prototype/Assets/Scripts/SimpleController.cs
+++ Jam5-Prototype/Assets/Scripts/SimpleController.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 public class SimpleController : MonoBehaviour
 {
 
-	private float speed = 2f;
-	private Rigidbody2D rb2d;
+
+    private float speed;
     private bool disabled = false;
     private bool carrying = false;
     private bool boosting = false;
+    private Rigidbody2D rb2d;
 
-	[SerializeField] private int index;
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float h = Input.GetAxis("Horizontal" + index);
-		float v = Input.GetAxis("Vertical" + index);
-		rb2d.velocity = new Vector2( h * speed,v *speed);
+	
         SetSpeed();
 	}
 
