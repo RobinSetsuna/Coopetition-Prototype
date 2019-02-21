2acc062c3e75ef1244767b2841b258e2be4e62a9
IllianaAvenon <cdb6474@g.rit.edu>
2019-02-21 10:16:10 -0500

    Player Added
    Added the first pass of the Player class. Whoever made the Simple Controller class, that was for sure the way I wanted to set it up, so thanks for making my life a bit easier this morning.

---
+++ Jam5-Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private bool boosting = false;
+    [SerializeField] private bool onChair = false;
+    [SerializeField] private bool carrying = false;
+    [SerializeField] private float sightRadius = 5.0f;
+    [SerializeField] private int playerIndex;
+    [SerializeField] private SimpleController myInput;
+    [SerializeField] private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        myInput = new SimpleController();
+        rb = myInput.GetRigidbody();        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      if(onChair)
+        {
+            myInput.SetDisabled(true);
+        }
+	}
+
+   public void SetBoosting(bool boost) { boosting = boost; myInput.SetBoosting(boosting); }
+   public void SetIndex(int index) { playerIndex = index; }
+   public int GetIndex() { return playerIndex; }
+   public void SetRadius(int overlapped)
+    {
+        sightRadius = 5.0f + ((1 + overlapped) * .25f); //Line of sight radius is increased based on how many other players are within range of this player 
+    }
+}
--- Jam5-Prototype/Assets/Scripts/SimpleController.cs
+++ Jam5-Prototype/Assets/Scripts/SimpleController.cs
@@ -7,6 +7,9 @@ public class SimpleController : MonoBehaviour
 
 	private float speed = 2f;
 	private Rigidbody2D rb2d;
+    private bool disabled = false;
+    private bool carrying = false;
+    private bool boosting = false;
 
 	[SerializeField] private int index;
 	// Use this for initialization
@@ -20,5 +23,35 @@ public class SimpleController : MonoBehaviour
 		float h = Input.GetAxis("Horizontal" + index);
 		float v = Input.GetAxis("Vertical" + index);
 		rb2d.velocity = new Vector2( h * speed,v *speed);
+        SetSpeed();
 	}
+
+   public Rigidbody2D GetRigidbody()
+    {
+        return rb2d;
+    }
+
+    /// <summary>
+    /// Determines whether the controller is active (based on chair)
+    /// </summary>
+    /// <param name="useable"></param>
+    public void SetDisabled(bool useable)
+    {
+        disabled = useable;
+    }
+    public void SetCarry(bool carry)
+    {
+        carrying = carry;
+    }
+    void SetSpeed()
+    {
+        if (carrying) { speed = 1.5f; }
+        else if (boosting) {speed = 2.5f; }
+        else { speed = 2.0f; }
+    }
+
+    public void SetBoosting(bool boosting)
+    {
+        this.boosting = boosting;
+    }
 }
