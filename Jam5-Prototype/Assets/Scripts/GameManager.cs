9bca9369683e2887e568223c5dad2cadb4e92db8
robinsetsuna <robinoneword@outlook.com>
2019-02-23 17:31:27 -0500

    Add Basic UI

--- Jam5-Prototype/Assets/Scripts/GameManager.cs
+++ Jam5-Prototype/Assets/Scripts/GameManager.cs
@@ -33,6 +33,11 @@ public class GameManager : MonoBehaviour {
     private int roundIndex;
     private bool isChairTimerOn;
     public GameObject blackMask;
+    public GameObject roundText;
+    public GameObject roundResultText;
+    public GameObject info;
+    public GameObject title;
+    public GameObject buttonText;
 
     UnityEvent DropChair;
     UnityEvent Highlight;
@@ -71,6 +76,7 @@ public class GameManager : MonoBehaviour {
                 switch (currentGameState)
                 {
                     case GameState.Initial:
+                        roundText.GetComponent<Text>().text = "Round" + roundIndex;
                         chairHoldTime = initialChairHoldTime;
                         isChairTimerOn = false;
                         PlayerOnChair = null;
@@ -94,16 +100,21 @@ public class GameManager : MonoBehaviour {
                         roundIndex += 1;
                         if (roundIndex % (totalRounds + 1) == 0)
                         {
-                            //Game Over
-                            //Show total scores and the winner!\
+                            int max = 0;
+                            string winner = "nobody";
+                            foreach(KeyValuePair<string,int> p in playerScore)
+                            {
+                                if (p.Value > max)
+                                {
+                                    max = p.Value;
+                                    winner = p.Key;
+                                }
+                            }
+                            ShowScore("Winner is " + winner + "!");
                         }
                         else
                         {
-                            Debug.Log("player0 score:"+ playerScore["Player0"]);
-                            Debug.Log("player1 score:" + playerScore["Player1"]);
-                            Debug.Log("player2 score:" + playerScore["Player2"]);
-                            Debug.Log("player3 score:" + playerScore["Player3"]);
-                            //CurrentGameState = GameState.Initial;
+                            ShowScore("Round End");
                         }
                         break;
                 }
@@ -112,6 +123,8 @@ public class GameManager : MonoBehaviour {
     }
     void Start()
     {
+        //initial current round index
+        roundIndex = 4;
         //set initial game state
         CurrentGameState = GameState.Initial;
         //initial player score 
@@ -122,8 +135,7 @@ public class GameManager : MonoBehaviour {
             { "Player2", 0 },
             { "Player3", 0 }
         };
-        //initial current round index
-        roundIndex = 1;
+        
         
         LightAura._instance.Enable();
     }
@@ -246,7 +258,7 @@ public class GameManager : MonoBehaviour {
             }
             timer -= Time.deltaTime;
             yield return new WaitForEndOfFrame();
-            GameObject.Find("TextTotalTime").GetComponent<Text>().text = timer.ToString();
+            GameObject.Find("TextTotalTime").GetComponent<Text>().text = "Time Left: " + (int)timer;
         }
         if(currentGameState != GameState.End)
             CurrentGameState = GameState.End;
@@ -257,6 +269,23 @@ public class GameManager : MonoBehaviour {
     {
         playerScore[playerOnChair] += 1;
         playerScore[playerCarryChair] += 2;
+    }
+    private void ShowScore(string t)
+    {
+        //Debug.Log("player0 score:"+ playerScore["Player0"]);
+        //Debug.Log("player1 score:" + playerScore["Player1"]);
+        //Debug.Log("player2 score:" + playerScore["Player2"]);
+        //Debug.Log("player3 score:" + playerScore["Player3"]);
+        if (roundIndex % (totalRounds + 1) == 0)
+        {
+            buttonText.GetComponent<Text>().text = "Quit Game";
+        }
+        title.GetComponent<Text>().text = t;
+        roundResultText.GetComponent<Text>().text = "Player0 Score:" + playerScore["Player0"] + "\n" +
+                                                    "Player1 Score:" + playerScore["Player1"] + "\n" +
+                                                    "Player2 Score:" + playerScore["Player2"] + "\n" +
+                                                    "Player3 Score:" + playerScore["Player3"];
+        roundResultText.SetActive(true);
     }
 
     ///////////////// For other script to call///////////////////////
@@ -271,6 +300,7 @@ public class GameManager : MonoBehaviour {
         if(!isChairTimerOn)
             StartCoroutine(ChairTimer());
         playerCarryChair = p;
+        StartCoroutine(WaitForSeconds(5f, p + " Carrys the Chair!"));
         
     }
     public void SitOnChair(string p)
@@ -278,11 +308,32 @@ public class GameManager : MonoBehaviour {
         if (CurrentGameState != GameState.Battle)
             CurrentGameState = GameState.Battle;
         playerOnChair = p;
+        StartCoroutine(WaitForSeconds(5f, p + " Sits on the Chair!"));
     }
 
     public void PlayerHit()
     {
         if(isChairTimerOn)
             chairHoldTime -= chairHoldTimeDecOnHit;
+    }
+    public void NextRound()
+    {
+        if (roundIndex % (totalRounds + 1) == 0)
+        {
+            QuitGame();
+        }
+        else
+        {
+            CurrentGameState = GameState.Initial;
+            roundResultText.SetActive(false);
+        }          
+    }
+    IEnumerator WaitForSeconds(float f, string text)
+    {
+        info.GetComponent<Text>().text = text;
+        info.SetActive(true);
+        yield return new WaitForSecondsRealtime(f);
+        info.SetActive(false);
+        yield return null;
     }
 }
