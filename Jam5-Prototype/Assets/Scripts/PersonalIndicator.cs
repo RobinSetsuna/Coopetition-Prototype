dd746ed56e0e02669f47165b63dfc5dbbba4fe19
robinsetsuna <robinoneword@outlook.com>
2019-02-24 17:24:58 -0500

    Make Indicator disappear when transit to battle

--- Jam5-Prototype/Assets/Scripts/PersonalIndicator.cs
+++ Jam5-Prototype/Assets/Scripts/PersonalIndicator.cs
@@ -70,9 +70,12 @@ public class PersonalIndicator : MonoBehaviour
             {
                 if (!Indicator.GetComponent<SpriteRenderer>().enabled)
                 {
-                    Indicator.GetComponent<SpriteRenderer>().enabled = true;
+                    if (GameManager.Instance.CurrentGameState == GameState.Battle)
+                        Indicator.GetComponent<SpriteRenderer>().enabled = false;
+                    else
+                        Indicator.GetComponent<SpriteRenderer>().enabled = true;
                 }
-                var YChange = Mathf.Clamp(distance/12, 0.0f,2.0f);
+                    var YChange = Mathf.Clamp(distance/12, 0.0f,2.0f);
                 var ScaleChange = Mathf.Clamp(4f/(distance*10), 0f, 0.2f);
                 var ratio = Mathf.Clamp(100f/(distance), 2f, 4f);
                 ScaleChange = Mathf.SmoothDamp(Indicator.transform.localScale.y, ScaleChange + originalScale, ref velocity,
@@ -97,8 +100,12 @@ public class PersonalIndicator : MonoBehaviour
             {
                 if (!Indicator.GetComponent<SpriteRenderer>().enabled)
                 {
-                    Indicator.GetComponent<SpriteRenderer>().enabled = true;
+                    if (GameManager.Instance.CurrentGameState == GameState.Battle)
+                        Indicator.GetComponent<SpriteRenderer>().enabled = false;
+                    else
+                        Indicator.GetComponent<SpriteRenderer>().enabled = true;
                 }
+                Indicator.GetComponent<SpriteRenderer>().enabled = false;
                 var YChange = Mathf.Clamp(distance/12, 0.0f,2.0f);
                 var ScaleChange = Mathf.Clamp(4f/(distance*10), 0f, 0.2f);
                 var ratio = Mathf.Clamp(100f/(distance), 2f, 4f);
